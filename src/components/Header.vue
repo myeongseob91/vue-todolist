6f27e48e3735a3114603eb621dd1273722d79a06
cppc-PC\cppc <myeongseob91@naver.com>
2019-02-24 15:15:05 +0900

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{msg}}</p>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'Hell O!'
+      }
+    }
+  }
+</script>
+
+<style>
+  p {
+    font-size: 2em;
+    text-align: center;
+  }
+</style>
